de0f2d6fcb428f4b6ce217f331cfcee599fbe2c3
Githubmaldito <marcusrms307@gmail.com>
2026-01-16 16:36:45 -0300

    cron

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import "dotenv/config";
 // importando as variáveis de ambiente do arquivo .env
-
 import cors from 'cors';//importando o middleware CORS para permitir requisições de diferentes origens
 import express from 'express';
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
+import job from './lib/cron.js'; //importando o cron job para manter o servidor ativo
 
 const app = express();
 // usando .env.PORT para definir a porta dinamicamente
 const PORT = process.env.PORT || 3000;
+
+job.start(); //iniciando o cron job
 
 //middleware para interpretar JSON nas requisições
 app.use(express.json());
---
+++ src/lib/cron.js
@@ -0,0 +1,18 @@
+import cron from "cron";
+import https from "https";
+
+//coisa pra fazer com que o servidor nao pare
+const job = new cron.CronJob("*/13 * * * *", function () {
+  https
+    .get(process.env.API_URL, (res) => {
+      if (res.statusCode === 200) console.log("GET request sent successfully");
+      else console.log("GET request failed", res.statusCode);
+    })
+    .on("error", (e) => console.error("Error while sending request", e));
+});
+
+export default job;
+
+// CRON JOB EXPLANATION:
+// Cron jobs are scheduled tasks that run periodically at fixed intervals
+// we want to send 1 GET request for every 14 minutes
